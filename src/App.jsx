bef3a876324e44673da1347c6a0efbb25fc3fac0
Gabriel Castro <gabrielcssg@gmail.com>
2024-06-05 15:19:16 -0300

    feat: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
-import './global.css'
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post />
-      <Post />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author={'Gabriel Castro'} content={'testee'} />
+          <Post author={'Gabriel Castro'} content={'testee'} />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1573496773905-f5b17e717f05?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/gabrielcsg.png"
+        />
+        <strong>Gabriel Castro</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
