321b0e3ee83a5c564db1f5f4bdcfa4c1a7b3652d
Gabriel Castro <gabrielcssg@gmail.com>
2024-06-26 23:05:33 -0300

    feat: comments and avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,14 @@
 import styles from './Post.module.css';
 
+import { Comment } from './Comment';
+import { Avatar } from './Avatar';
+
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/gabrielcsg.png"
-          />
+          <Avatar src="https://github.com/gabrielcsg.png" />
           <div className={styles.authorInfo}>
             <strong>Gabriel Castro</strong>
             <span>Web Developer</span>
@@ -46,6 +46,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   );
 }
